607d63d81e090c7074cdb3b26d945f68156bf523
Lucas Almeida <lucas.almeida@zup.com.br>
2023-12-29 15:16:12 -0300

    return public access configuration

--- templates-deploy/main.tf
+++ templates-deploy/main.tf
@@ -3,20 +3,28 @@ resource "aws_s3_bucket" "b" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket_ownership_controls" "b_ownership_controls" {
+  bucket = aws_s3_bucket.b.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "b_public_access_block" {
   bucket = aws_s3_bucket.b.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_acl" "b_acl" {
   depends_on = [
+    aws_s3_bucket_ownership_controls.b_ownership_controls,
     aws_s3_bucket_public_access_block.b_public_access_block,
   ]
 
   bucket = aws_s3_bucket.b.id
-  acl    = "private"
+  acl    = "public-read"
 }
